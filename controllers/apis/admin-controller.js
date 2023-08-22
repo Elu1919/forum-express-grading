2f83812100aa7e97563561c38c29ad3331ec39b2
Elu1919 <qw29149974@gmail.com>
2023-08-22 22:13:16 +0800

    feat:  add adminServices.createRestaurants

--- controllers/apis/admin-controller.js
+++ controllers/apis/admin-controller.js
@@ -10,6 +10,9 @@ const adminController = {
   },
   postRestaurant: (req, res, next) => {
     adminServices.postRestaurant(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  },
+  createRestaurant: (req, res, next) => {
+    adminServices.createRestaurant(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 
